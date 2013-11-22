fe8e453499ceb49b3a52e55b1dd1c8d7cd667b3e
Kai Wells <me@quells.net>
2013-11-21 20:09:16 -0600

    Initial Release
    Just the workflow and the script, README is on its way.

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-pinboard-alfred2
-================
+Pinboard in Alfred 2
+====================
+
+A Python script to display Pinboard bookmarks in the Alfred window. You will need Alfred 2 and a regular Pinboard account to use this.
+
+# Installation
+
+To install the Pinboard workflow, double click on ```Pinboard.alfredworkflow``` or drag the workflow to the workflow window in Alfred.
+
+Find your API token on the [Pinboard settings page](https://pinboard.in/settings/password).
+
+Next, edit the script filter by double clicking on it. Edit the line ```print list("TOKEN", 3600, "{query}")``` to fill in your API token and caching preference. The default value of 3600 means that the script will only try to cache your bookmarks if it has not done so in the last hour (3600 seconds). A value of 0 means that the script will never write to disk.
+
+# How to use
+
+Simply type ```pinb``` (or whatever you configure) into Alfred. If your bookmarks are cached, they should appear in Alfred almost immediately. If not, downloading from the Pinboard server could take some time.
+
+Hitting ```enter``` while a bookmark is selected will open the URL associated with that bookmark in Safari. Holding ```alt``` while doing this will open the URL in Google Chrome.
+
+To filter the results, add your query after a space. For example, ```pinb apple``` will only display results with ```apple``` in the title, description, or tags of the bookmark. This is case insensitive and is compatible with regular expression syntax, if you’re into that. The first result will open the Pinboard search page with the results of the query.
+
+# Disclaimers
+
+This script and associated Alfred workflow is released under the [MIT license](https://github.com/quells/pinboard-alfred2/blob/master/LICENSE).
+
+The Pinboard name and logo are owned by Nine Fives Software. Kai Wells does not own or claim to own anything related to Pinboard.
+
+# Version History
+
+## 1.0 - November 21, 2013
+
+- Initial release
---
+++ pinboard.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python
+
+from xml.etree import ElementTree as ET
+import urllib, urllib2, json
+import glob
+import re
+import time, calendar
+
+def list(TOKEN, CACHETIME, QUERY = ""):
+	# CHECK FOR RECENCY
+	if CACHETIME != 0:
+		try:
+			with open('timestamp.txt') as f:
+				t = f.read()
+			f.close()
+			if int(t) < 1:
+				f = open('timestamp.txt', 'w')
+				f.write(`calendar.timegm(time.gmtime())`)
+				f.close()
+		except IOError:
+			f = open('timestamp.txt', 'w')
+			t = calendar.timegm(time.gmtime())
+			f.write(`t`)
+			f.close()
+	# PARSE ITEMS
+	xml = []
+	if not QUERY == "":
+		xml.append ({
+					'title': 'Search for ' + `QUERY`,
+					'arg': 'https://pinboard.in/search/u:' + TOKEN.split(':')[0] + '?query=' + QUERY.replace(' ', '+') + '&fulltext=on',
+					'icon': 'blue-100.png'
+				})
+	if calendar.timegm(time.gmtime()) - int(t) > CACHETIME or CACHETIME == 0:
+		# GET JSON DATA
+		url = 'https://api.pinboard.in/v1/posts/all?format=json&auth_token=' + TOKEN
+		response = urllib2.urlopen(url)
+		response_items = response.read()
+		items = json.loads(response_items)
+		for i in range(len(items)-1):
+			item = items[i]
+			if CACHETIME != 0:
+				try:
+					with open('bookmarks/' + item[u'time'] + '.txt') as f:
+						pass
+					f.close()
+				except IOError:
+					f = open('bookmarks/' + item[u'time'] + '.txt', 'w')
+					f.write(item[u'description'].encode('utf8') + '\t' + item[u'extended'].encode('utf8') + '\t' + item[u'href'].encode('utf8') + '\t' + item[u'tags'].encode('utf8'))
+					f.close()
+			if re.search(re.compile(QUERY, re.IGNORECASE), item[u'description']) or re.search(re.compile(QUERY, re.IGNORECASE), item[u'extended']) or re.search(re.compile(QUERY, re.IGNORECASE), item[u'tags']):
+				subtitletext = item[u'extended'] + " | " + item[u'tags'] if not item[u'extended'] == "" else item[u'tags']
+				xml.append ({
+					'title': item[u'description'],
+					'subtitle': subtitletext,
+					'arg': item[u'href'],
+					'icon': 'blue-' + `100-(10*i/len(items))*10` + '.png'
+				})
+	else:
+		# READ BOOKMARKS FROM DISK
+		items = []
+		for h in glob.glob('bookmarks/*.txt'):
+			with open(h) as f:
+				t = f.read()
+			f.close()
+			t = t.split('\t')
+			items.append(t)
+		items = items[::-1]
+		for i in range(len(items)-1):
+			item = items[i]
+			if re.search(re.compile(QUERY, re.IGNORECASE), item[0]) or re.search(re.compile(QUERY, re.IGNORECASE), item[1]) or re.search(re.compile(QUERY, re.IGNORECASE), item[3]):
+				subtitletext = item[1] + " | " + item[3] if not item[1] == "" else item[3]
+				xml.append({
+					'title': item[0].decode('utf8'),
+					'subtitle': subtitletext.decode('utf8'),
+					'arg': item[2].decode('utf8'),
+					'icon': 'blue-' + `100-(10*i/len(items))*10` + '.png'
+				})
+	# PLANT XML TREE
+	xml_items = ET.Element('items')
+	for item in xml:
+		xml_item = ET.SubElement(xml_items, 'item')
+		for key in item.keys():
+			if key is 'uid' or key is 'arg':
+				xml_item.set(key, item[key])
+			else:
+				child = ET.SubElement(xml_item, key)
+				child.text = item[key]
+	return ET.tostring(xml_items)
